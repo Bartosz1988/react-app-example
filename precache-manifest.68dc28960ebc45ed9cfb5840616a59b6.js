257f0ad299af2271ad9a09b9fa230d1d81f15b56
Bartek Sieminski <b.r.sieminski@gmail.com>
2020-07-02 19:22:47 +0200

    Updates

---
+++ precache-manifest.68dc28960ebc45ed9cfb5840616a59b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "add9467de19790d38ebc",
+    "url": "/react-app-example/static/js/runtime~main.2ed37660.js"
+  },
+  {
+    "revision": "4795f64ee8b22c95803f",
+    "url": "/react-app-example/static/js/main.28f843f5.chunk.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/react-app-example/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "4795f64ee8b22c95803f",
+    "url": "/react-app-example/static/css/main.c1e9f0e4.chunk.css"
+  },
+  {
+    "revision": "48f1d0848c301aff912b04ebdd474434",
+    "url": "/react-app-example/index.html"
+  }
+];
